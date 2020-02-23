96448914e8fcb795d7ad44f620a27ca8f6530021
Mustafa Genc <m.genc@2016.ljmu.ac.uk>
2020-02-23 23:30:47 +0000

    Changed text colour of cleaning service activity and referenced some code

--- app/src/main/java/com/example/prototype/MainActivity.java
+++ app/src/main/java/com/example/prototype/MainActivity.java
@@ -26,22 +26,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openAdminMenu(); // creates main menu method name
-            }
-        });
+//        button2 = (Button) findViewById(R.id.button2);
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openAdminMenu(); // creates Admin menu method name
+//            }
+//        });
     }
 
-    public void openMainMenu() { //method created for main menu
+    public void openMainMenu() { //class created for main menu
         Intent intent = new Intent(this, MainMenu.class);
         startActivity(intent);
     }
 
-    public void openAdminMenu() { //method created for main menu
-        Intent intent = new Intent(this, AdminMenu.class);
-        startActivity(intent);
-    }
+//    public void openAdminMenu() { //class created for admin menu
+//        Intent intent = new Intent(this, AdminMenu.class);
+//        startActivity(intent);
+//    }
 }
