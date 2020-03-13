ad1272bd8234dfda2ef211d1aad58f838646363a
Mustafa Genc <m.genc@2016.ljmu.ac.uk>
2020-03-13 07:47:29 +0000

    Connected Firebase to guest application with same package of admin project.

--- app/src/main/java/com/example/prototype/MainActivity.java
+++ app/src/main/java/com/example/prototype/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this, "Firebase connection success",Toast.LENGTH_LONG).show(); // shows message that firebase connection was successful.
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
