4a8106abdbcfbcd071092e1c4707c85813f1cac3
Mustafa Genc <m.genc@2016.ljmu.ac.uk>
2020-03-23 11:39:18 +0000

    Added a login screen. Also made some buttons to display a text that they are not available for use.

--- app/src/main/java/com/example/prototype/MainActivity.java
+++ app/src/main/java/com/example/prototype/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(MainActivity.this, "Firebase connection success",Toast.LENGTH_LONG).show(); // shows message that firebase connection was successful.
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/prototype/MainMenu.java
+++ app/src/main/java/com/example/prototype/MainMenu.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
 
-    private ImageButton roomservice;
+    private ImageButton roomservice, transportation, foodservice, maintenance, checkout, baggagecollection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +19,59 @@ public class MainMenu extends AppCompatActivity {
         getSupportActionBar().setTitle("MainMenu");
 
         roomservice = (ImageButton) findViewById(R.id.roomservice);
+        transportation = (ImageButton) findViewById(R.id.transportation);
+        foodservice = (ImageButton) findViewById(R.id.foodservice);
+        maintenance = (ImageButton) findViewById(R.id.maintenance);
+        checkout = (ImageButton) findViewById(R.id.checkout);
+        baggagecollection = (ImageButton) findViewById(R.id.baggagecollection);
+
         roomservice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openRoomServiceMenu(); // creates main menu method name
             }
         });
+
+        transportation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainMenu.this, "This action is unavailable", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        foodservice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainMenu.this, "This action is unavailable", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        maintenance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainMenu.this, "This action is unavailable", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        checkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainMenu.this, "This action is unavailable", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        baggagecollection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainMenu.this, "This action is unavailable", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
     public void openRoomServiceMenu() {
         Intent intent = new Intent(this, RoomServiceMenu.class);
         startActivity(intent);
     }
+
 }
